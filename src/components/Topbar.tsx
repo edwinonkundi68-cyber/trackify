e0304698345211e6b30e2bbb51c308b37f3272af
Edwin Onkundi <edwinonkundi68@gmail.com>
2026-04-11 10:52:24 +0300

    Day 9: Event handlers and search input

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 function Topbar(){
+    const[searchText,setSearchText]=useState('')
+    function handleSearch(e:React.ChangeEvent<HTMLInputElement>){
+        setSearchText(e.target.value)
+    }
     return(
         <div>
             <h3>Dashbord</h3>
-            <input type="text" placeholder="search...." />
-            <span>EO</span>
+            <input type="text"
+            placeholder="Search..."
+            onChange={handleSearch}
+             />
+             <span>EO</span>
+             <p>You searched:{searchText}</p>
         </div>
     )
 }
